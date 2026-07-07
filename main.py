092605016a7c4051cf594db1ede18b89d7133544
Behram Usuk <behrammusuk@gmail.com>
2026-07-07 14:52:16 +0300

    chore: release hazirliklari tamamlandi, uygulama ikonu eklendi ve path ayarlari yapildi

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import sys
+import os
 from PySide6.QtWidgets import QApplication
 from gui.main_window import MainWindow
+
+def resource_path(relative_path):
+    """ PyInstaller'ın oluşturduğu geçici klasörün (MEIPASS) yolunu bulur. """
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 def main():
     app = QApplication(sys.argv)
     
-    # QSS  dosyasını okuyup tüm uygulamaya giydirdik
-    try:
-        with open("gui/styles.qss", "r", encoding="utf-8") as file:
-            app.setStyleSheet(file.read())
-    except FileNotFoundError:
-        print("Uyarı: styles.qss dosyası bulunamadı, varsayılan tema ile başlatılıyor.")
+    # Tema dosyasını artık dinamik yolla okuyoruz
+    qss_path = resource_path("gui/styles.qss")
     
+    try:
+        with open(qss_path, "r", encoding="utf-8") as f:
+            app.setStyleSheet(f.read())
+    except FileNotFoundError:
+        print(f"Uyarı: Tema dosyası bulunamadı: {qss_path}")
+
     window = MainWindow()
     window.show()
-    
     sys.exit(app.exec())
 
 if __name__ == "__main__":
